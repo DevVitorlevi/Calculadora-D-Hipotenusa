07f44208670e42b4a9e7529d0b7835de50591387
Dev Vitor <160429477+Dev-Vitor-lev@users.noreply.github.com>
2024-08-14 19:47:06 -0300

    att

--- Hipotenusa/script.js
+++ Hipotenusa/script.js
@@ -1,4 +1,16 @@
+document.addEventListener('keydown',e =>{
+
+    if(e.key== 'Escape'){
+
+        document.getElementById('oposto').value=""
+        document.getElementById('adjacente').value=""
+        document.querySelector('.res').innerHTML="Resultado:"
+
+    }
+})
+
 document.addEventListener('keypress', e => {
+
     if(e.key == 'Enter'){
        
         var btn = document.getElementById('button')
@@ -12,14 +24,35 @@ document.addEventListener('keypress', e => {
             res.innerHTML="Calculando..."
             res.innerHTML+=`<br> A Soma Dos Quadrados de <strong>${ cOp}</strong> e <strong>${cAd}</strong> é igual a Hipotenusa<strong> ${Math.round(hip)}</strong>` 
         }
+
         else{
-            alert("Insira Dados")
             res.innerHTML="Imposivel Calcular"
         }
+        
     }   
-  
 })
+
+
+function Calcular(){
+           
+            let cOp = parseFloat(document.getElementById('oposto').value)
+            let cAd = parseFloat(document.getElementById('adjacente').value)
+            let res =  document.querySelector('.res')
+    
+            if(!isNaN(cOp) && !isNaN(cAd)){
+                const hip = Math.sqrt(Math.pow( cOp, 2) + Math.pow(cAd, 2));
+                res.innerHTML="Calculando..."
+                res.innerHTML+=`<br> A Soma Dos Quadrados de <strong>${ cOp}</strong> e <strong>${cAd}</strong> é igual a Hipotenusa<strong> ${Math.round(hip)}</strong>` 
+            }
+            else{
+                
+                res.innerHTML="Imposivel Calcular"
+            }
+            
+        }   
+ 
 function Limpar(){
+
     document.getElementById('oposto').value=""
     document.getElementById('adjacente').value=""
     document.querySelector('.res').innerHTML="Resultado:"
