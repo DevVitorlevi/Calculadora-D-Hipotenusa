5168353bae811ebda6f88ecc473ed7b29c8e9d12
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-08-25 19:46:58 -0300

    att

--- Hipotenusa/script.js
+++ Hipotenusa/script.js
@@ -2,33 +2,14 @@ document.addEventListener('keydown',e =>{
 
     if(e.key== 'Escape'){
 
-        document.getElementById('oposto').value=""
-        document.getElementById('adjacente').value=""
-        document.querySelector('.res').innerHTML="Resultado:"
-
+        Limpar();
     }
-})
-
+})  
 document.addEventListener('keypress', e => {
 
     if(e.key == 'Enter'){
        
-        var btn = document.getElementById('button')
-        btn.click()
-        let cOp = parseFloat(document.getElementById('oposto').value)
-        let cAd = parseFloat(document.getElementById('adjacente').value)
-        let res =  document.querySelector('.res')
-
-        if(!isNaN(cOp) && !isNaN(cAd)){
-            const hip = Math.sqrt(Math.pow( cOp, 2) + Math.pow(cAd, 2));
-            res.innerHTML="Calculando..."
-            res.innerHTML+=`<br> A Soma Dos Quadrados de <strong>${ cOp}</strong> e <strong>${cAd}</strong> é igual a Hipotenusa<strong> ${Math.round(hip)}</strong>` 
-        }
-
-        else{
-            res.innerHTML="Imposivel Calcular"
-        }
-        
+        Calcular(); 
     }   
 })
 
