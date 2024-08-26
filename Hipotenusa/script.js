188cd1e725222e313236c34c5c4323470eb9e11a
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-08-26 13:55:39 -0300

    att

--- Hipotenusa/script.js
+++ Hipotenusa/script.js
@@ -8,30 +8,31 @@ document.addEventListener('keydown',e =>{
 document.addEventListener('keypress', e => {
 
     if(e.key == 'Enter'){
-       
+    
         Calcular(); 
     }   
 })
 
 
 function Calcular(){
-           
             let cOp = parseFloat(document.getElementById('oposto').value)
             let cAd = parseFloat(document.getElementById('adjacente').value)
             let res =  document.querySelector('.res')
-    
-            if(!isNaN(cOp) && !isNaN(cAd)){
+
+            if(isNaN(cAd) || isNaN(cOp)){
+                    res.innerHTML='Falta Dados'
+            }
+            else{
+                if(cAd>0||cOp>0){
                 const hip = Math.sqrt(Math.pow( cOp, 2) + Math.pow(cAd, 2));
                 res.innerHTML="Calculando..."
                 res.innerHTML+=`<br> A Soma Dos Quadrados de <strong>${ cOp}</strong> e <strong>${cAd}</strong> é igual a Hipotenusa<strong> ${Math.round(hip)}</strong>` 
+                }
+            else if(cOp<=0 || cAd <=0){
+                res.innerHTML="Imposivel Calcular. Numeros Negativos ou Nulo"}
             }
-            else{
-                
-                res.innerHTML="Imposivel Calcular"
-            }
-            
-        }   
- 
+        }
+
 function Limpar(){
 
     document.getElementById('oposto').value=""
